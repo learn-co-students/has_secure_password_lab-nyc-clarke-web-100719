8f040bcd852dee430895f449d4040880a4ef98bd
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-11-11 01:15:49 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'home', to: 'welcome#home'
+
+  get 'signup', to: 'users#new'
+  get 'users', to: 'users#index'
+  post 'users', to: 'users#create'
+
+
+  get 'login', to: 'sessions#new'
+  post 'sessions', to: 'sessions#create'
+  delete 'sessions', to: 'sessions#destroy'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
